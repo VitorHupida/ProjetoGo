db4b5886da1f812284a7dac333f254526a0aa358
VitorHupida <vitopierinidantas@hotmail.com>
2025-01-11 22:23:20 -0300

    Adicionando rotas lógicas

--- router/router.go
+++ router/router.go
@@ -3,14 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Inicializador() {
+	println()
 	//inicializa o Router utilizando as configurações default do GIN
 	router := gin.Default()
 	//define uma rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	inicilizandoRotas(router)
 	//roda a API
 	router.Run(":8080")
 }
